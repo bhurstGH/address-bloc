38063713881e16a35bdb43806a5666341182c3fd
Brett Hurst <bretthurst@outlook.com>
2018-12-08 12:38:49 -0800

    Implemented search and delete functions.

--- controllers/ContactController.js
+++ controllers/ContactController.js
@@ -1,4 +1,3 @@
-const inquirer = require('inquirer');
 const Contact = require('../db/models').Contact;
 
 module.exports = class ContactController {
@@ -22,9 +21,88 @@ module.exports = class ContactController {
                 }
             }
         ];
+
+        this.searchQuestions = [
+            {
+                type: "input",
+                name: "name",
+                message: "Name of contact to search - ",
+                validate(val){
+                    return val !== "";
+                }
+            }
+        ];
+
+        this.showContactQuestions = [
+            {
+                type: "list",
+                name: "selected",
+                message: "Please choose from an option below: ",
+                choices: [
+                    "Delete contact",
+                    "Main menu"
+                ]
+            }
+        ];
+
+        this.deleteConfirmQuestions = [
+            {
+                type: "confirm",
+                name: "confirmation",
+                message: "Are you sure you want to delete this contact?"
+            }
+        ];
+        
     }
 
     addContact(name, phone) {
         return Contact.create({name, phone});
     }
+
+    getContacts() {
+        return Contact.findAll();
+    }
+
+    iterativeSearch(contacts, target) {
+        for (let contact of contacts) {
+            if (contact.name.toLowerCase() === target.toLowerCase()) {
+                return contact;
+            }
+        }
+        return null;
+    }
+
+    binarySearch(contacts, target) {
+        var min = 0;
+        var max = contacts.length - 1;
+        var mid;
+
+        while (min <= max) {
+            mid = Math.floor((min + max) / 2);
+            if (contacts[mid].name > target) {
+                max = mid -1;
+            }
+            else if (contacts[mid].name < target) {
+                min = mid + 1;
+            }
+            else {
+                return contacts[mid];
+            }
+        }
+
+        return null;
+    }
+
+    search(name) {
+        return Contact.findOne({
+            where: {name}
+        });
+    }
+
+    delete(id) {
+        return Contact.destroy({
+            where: {id}
+        })
+    }
+
 }
--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -10,6 +10,8 @@ module.exports = class MenuController {
                 message: "Please choose from an option below: ",
                 choices: [
                     "Add new contact",
+                    "View all contacts",
+                    "Search for a contact",
                     "Exit"
                 ]
             }
@@ -23,6 +25,12 @@ module.exports = class MenuController {
             switch(response.mainMenuChoice){
                 case "Add new contact":
                     this.addContact();
+                    break;
+                case "View all contacts":
+                    this.getContacts();
+                    break;
+                case "Search for a contact":
+                    this.search();
                     break;
                 case "Exit":
                     this.exit()
@@ -53,6 +61,85 @@ module.exports = class MenuController {
         });
     }
 
+    getContacts(){
+        this.clear();
+
+        this.book.getContacts().then( contacts => {
+            for (let contact of contacts) {
+                this._printContact(contact);
+            }
+            this.main();
+        }).catch( err => {
+            console.log(err);
+            this.main();
+        })
+    }
+
+    search(){
+        inquirer.prompt(this.book.searchQuestions)
+        .then( target => {
+            this.book.search(target.name)
+            .then( contact => {
+                if (contact === null) {
+                    console.log("contact not found");
+                    this.search();
+                } else {
+                    this.showContact(contact);
+                }
+            });
+        }).catch( err => {
+            console.log(err);
+            this.main();
+        });
+    }
+
+    showContact(contact) {
+        this._printContact(contact);
+        inquirer.prompt(this.book.showContactQuestions)
+        .then( answer => {
+            switch(answer.selected) {
+                case "Delete contact":
+                    this.delete(contact);
+                    break;
+                case "Main menu":
+                    this.main();
+                    break;
+                default:
+                    console.log("Something went wrong");
+                    this.showContact(contact);
+            }
+        }).catch( err => {
+            console.log(err);
+            this.showContact(contact);
+        });
+    }
+
+    delete(contact){
+        inquirer.prompt(this.book.deleteConfirmQuestions)
+        .then( answer => {
+            if (answer.confirmation) {
+                this.book.delete(contact.id);
+                console.log("Contact deleted!");
+                this.main();
+            } else {
+                confirm.log("Contact not deleted");
+                this.showContact(contact);
+            }
+        }).catch( err => {
+            console.log(err);
+            this.main();
+        })
+    }
+
+    _printContact(contact) {
+        console.log(`
+            name: ${contact.name}
+            phone: ${contact.phone}
+            email: ${contact.email}
+            ----------------
+        `)
+    }
+
     exit(){
         console.log("Thanks for using AddressBloc!");
         process.exit();
@@ -61,4 +148,5 @@ module.exports = class MenuController {
     getContactCount(){
         return this.contacts.length;
     }
+
 }
